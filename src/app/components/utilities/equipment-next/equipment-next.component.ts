e0d1d2291bcf9de3617e1c53f5b68bc273b2a076
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2025-01-14 22:56:09 +0100

    Update 14/01/2025 (3)
    + equipment next completed (to test);

--- src/app/components/utilities/equipment-next/equipment-next.component.ts
+++ src/app/components/utilities/equipment-next/equipment-next.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ApplicationRef, Component, ComponentFactoryResolver, ComponentRef, Injector, Input } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog } from '@angular/material/dialog';
 import { Damage, Item } from 'src/app/models/item';
 import { CharacterService } from 'src/app/services/character.service';
 import { ManageEquipDialogComponent } from '../equipment/manage-equip-dialog/manage-equip-dialog.component';
 import { DiceRollerComponent } from '../dice-roller/dice-roller.component';
+import { SkillTooltipComponent } from '../skill-tooltip/skill-tooltip.component';
+import { ItemTooltipComponent } from '../item-tooltip/item-tooltip.component';
+import { ItemInfoSheetComponent } from '../inventory/item-info-sheet/item-info-sheet.component';
 
 @Component({
   selector: 'app-equipment-next',
@@ -13,21 +16,33 @@ import { DiceRollerComponent } from '../dice-roller/dice-roller.component';
   styleUrl: './equipment-next.component.scss'
 })
 export class EquipmentNextComponent {
-  constructor(private matDialog: MatDialog, private charService: CharacterService, private bottomSheet: MatBottomSheet) {}
+  constructor(
+    private matDialog: MatDialog,
+    private charService: CharacterService,
+    private bottomSheet: MatBottomSheet,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) { }
+
+  private tooltipRef: ComponentRef<ItemTooltipComponent> | null = null;
+
 
   public _char: any;
   public inventory: Item[] = [];
   public wearedItems: Item[] = [];
   public sets: any[] = [];
   public setIndex: number = 0;
-  public ammo: Item;
+  public ammos: Item[] = [];
+  public moreInfo: boolean = false;
+  public totalCA: number = 0;
 
   @Input() set char(char: any) {
     setTimeout(() => {
       this._char = char;
       this.inventory = this._char.equipaggiamento;
       this.sets = this._char.sets || [];
-
+      this.totalCA = this._char.CA + parseInt(this._char.CAShield || '0');
       this.setAmmo();
       this.checkWeared();
     }, 100);
@@ -36,6 +51,10 @@ export class EquipmentNextComponent {
   public _edit: boolean = false;
   @Input() set edit(edit: boolean) {
     this._edit = edit;
+  }
+
+  public toggleMoreInfo(): void {
+    this.moreInfo = !this.moreInfo;
   }
 
   public changeSet(): void {
@@ -141,14 +160,29 @@ export class EquipmentNextComponent {
       item.weared = true;
     });
 
+    this.charService.updateInventory(this._char.id, this.inventory);
+
     this.setAmmo();
   }
 
   private setAmmo(): void {
-    const ammo = this.inventory.find(item => item.category.toLowerCase() === 'munizioni' && item.weared);
+    const ammo = this.inventory.filter(item => item.category.toLowerCase() === 'munizioni');
     if (ammo) {
-      this.ammo = ammo;
+      this.ammos = ammo;
     }
+  }
+
+  public ammoAction(action: string, itemId: string): void {
+    const ammo = this.inventory.find(item => item.id === itemId);
+    switch (action) {
+      case 'add':
+        ammo.quantity += 1;
+        break;
+      case 'remove':
+        ammo.quantity -= 1;
+        break;
+    }
+    this.charService.updateInventory(this._char.id, this.inventory);
   }
 
   setColor(rarity: string): string {
@@ -173,90 +207,137 @@ export class EquipmentNextComponent {
     }
   }
 
-    public getDamagesString(formula, skill: string, extraDamages?: Damage[]) {
-      let skillMod: number = Math.floor((this._char.caratteristiche[skill] - 10) / 2) || 0;
-  
-      if (!formula) {
-        return { minimo: 'error', massimo: 'error' };
-      }
-      // Rimuovi gli spazi bianchi e separa la formula in termini
-      const termini = formula.replace(/\s/g, '').split('+');
-  
-      let minimo = 0;
-      let massimo = 0;
-  
-      // Calcola il minimo e il massimo per ogni termine
-      termini.forEach(termine => {
-        if (termine.includes('d')) {
-          // Se il termine contiene 'd', è un termine dei dadi
-          const [numDadi, numFacce] = termine.split('d').map(Number);
-          minimo += numDadi;
-          massimo += numDadi * numFacce;
-        } else {
-          // Altrimenti, è un termine costante
-          const costante = parseInt(termine);
-          minimo += costante;
-          massimo += costante;
-        }
-      });
-  
-      // Calcola il danno aggiuntivo, se presente
-      if (extraDamages && extraDamages.length > 0) {
-        extraDamages.forEach((extraDamage) => {
-          let extraMin = 0;
-          let extraMax = 0;
-          const extraTermini = extraDamage.formula.replace(/\s/g, '').split('+');
-          extraTermini.forEach(termine => {
-            if (termine.includes('d')) {
-              const [numDadi, numFacce] = termine.split('d').map(Number);
-              extraMin += numDadi;
-              extraMax += numDadi * numFacce;
-            } else {
-              const costante = parseInt(termine);
-              extraMin += costante;
-              extraMax += costante;
-            }
-          });
-          minimo += extraMin;
-          massimo += extraMax;
-        });
-      }
-  
-      if (minimo === massimo) return `${minimo}`;
-      return `${minimo + skillMod}-${massimo + skillMod}`;
-    }
-  
-    public getDiceFormula(formula, skill: string, extraDamages?: Damage[]): string {
-      const skillMod: number = Math.floor((this._char.caratteristiche[skill] - 10) / 2) || 0;
-      let resultFormula = formula;
-  
-      if (extraDamages && extraDamages.length > 0) {
-        extraDamages.forEach((extraDamage) => {
-          resultFormula += ` + ${extraDamage.formula}`;
-        });
-      }
-      return resultFormula + (skillMod > 0 ? `+${skillMod}` : '');
-    }
+  public getDamagesString(formula, skill: string, extraDamages?: Damage[]) {
+    let skillMod: number = Math.floor((this._char.caratteristiche[skill] - 10) / 2) || 0;
 
-    public getHitCheckString(index: number): string {
-      const set = this.sets[index];
-      const skillMod = Math.floor((this._char.caratteristiche[set.skill] - 10) / 2);
-      const bonusProficiency = this._char.tiriSalvezza.bonusCompetenza;
-      return `1d20${skillMod + bonusProficiency > 0 ? `+${skillMod + bonusProficiency}` : ''}`;
+    if (!formula) {
+      return { minimo: 'error', massimo: 'error' };
     }
+    // Rimuovi gli spazi bianchi e separa la formula in termini
+    const termini = formula.replace(/\s/g, '').split('+');
 
-      public rollFromSheet(formula: string, extra: string) {
-        this.matDialog.open(DiceRollerComponent, {
-          width: window.innerWidth < 768 ? '90%' : '500px',
-          autoFocus: false,
-          disableClose: true,
-          backdropClass: 'as-dialog-backdrop',
-          data: {
-            char: this._char,
-            formula: formula,
-            extra: extra
+    let minimo = 0;
+    let massimo = 0;
+
+    // Calcola il minimo e il massimo per ogni termine
+    termini.forEach(termine => {
+      if (termine.includes('d')) {
+        // Se il termine contiene 'd', è un termine dei dadi
+        const [numDadi, numFacce] = termine.split('d').map(Number);
+        minimo += numDadi;
+        massimo += numDadi * numFacce;
+      } else {
+        // Altrimenti, è un termine costante
+        const costante = parseInt(termine);
+        minimo += costante;
+        massimo += costante;
+      }
+    });
+
+    // Calcola il danno aggiuntivo, se presente
+    if (extraDamages && extraDamages.length > 0) {
+      extraDamages.forEach((extraDamage) => {
+        let extraMin = 0;
+        let extraMax = 0;
+        const extraTermini = extraDamage.formula.replace(/\s/g, '').split('+');
+        extraTermini.forEach(termine => {
+          if (termine.includes('d')) {
+            const [numDadi, numFacce] = termine.split('d').map(Number);
+            extraMin += numDadi;
+            extraMax += numDadi * numFacce;
+          } else {
+            const costante = parseInt(termine);
+            extraMin += costante;
+            extraMax += costante;
           }
         });
-      }
+        minimo += extraMin;
+        massimo += extraMax;
+      });
+    }
 
+    if (minimo === massimo) return `${minimo}`;
+    return `${minimo + skillMod}-${massimo + skillMod}`;
+  }
+
+  public getDiceFormula(formula, skill: string, extraDamages?: Damage[]): string {
+    const skillMod: number = Math.floor((this._char.caratteristiche[skill] - 10) / 2) || 0;
+    let resultFormula = formula;
+
+    if (extraDamages && extraDamages.length > 0) {
+      extraDamages.forEach((extraDamage) => {
+        resultFormula += ` + ${extraDamage.formula}`;
+      });
+    }
+    return resultFormula + (skillMod > 0 ? `+${skillMod}` : '');
+  }
+
+  public getHitCheckString(index: number): string {
+    const set = this.sets[index];
+    const skillMod = Math.floor((this._char.caratteristiche[set.skill] - 10) / 2);
+    const bonusProficiency = this._char.tiriSalvezza.bonusCompetenza;
+    return `1d20${skillMod + bonusProficiency > 0 ? `+${skillMod + bonusProficiency}` : ''}`;
+  }
+
+  public rollFromSheet(formula: string, extra: string) {
+    this.matDialog.open(DiceRollerComponent, {
+      width: window.innerWidth < 768 ? '90%' : '500px',
+      autoFocus: false,
+      disableClose: true,
+      backdropClass: 'as-dialog-backdrop',
+      data: {
+        char: this._char,
+        formula: formula,
+        extra: extra
+      }
+    });
+  }
+
+  public currentTooltipItem: any;
+  public showItemTooltip: boolean = false;
+  public tooltipPosition: { top: number | string, left: number | string } = { top: 0, left: 0 };
+
+  public showTooltip(event: MouseEvent, item: any) {
+    if (window.innerWidth < 768) return;
+    this.removeTooltip();
+    const factory = this.componentFactoryResolver.resolveComponentFactory(ItemTooltipComponent);
+    this.tooltipRef = factory.create(this.injector);
+    const tooltipElement = this.tooltipRef.location.nativeElement;
+    tooltipElement.classList.add('dynamic-tooltip');
+    this.tooltipRef.instance.item = item;
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const tooltipPosition = {
+      top: event.clientY + rect.height > window.innerHeight
+        ? window.innerHeight - rect.height - 10
+        : event.clientY,
+      left: event.clientX + 200,
+    };
+    tooltipElement.style.top = `${tooltipPosition.top}px`;
+    tooltipElement.style.left = `${tooltipPosition.left}px`;
+    this.appRef.attachView(this.tooltipRef.hostView);
+    const domElem = (this.tooltipRef.hostView as any).rootNodes[0] as HTMLElement;
+    document.body.appendChild(domElem);
+  }
+
+  public hideTooltip() {
+    this.removeTooltip();
+  }
+
+  private removeTooltip() {
+    if (this.tooltipRef) {
+      this.appRef.detachView(this.tooltipRef.hostView);
+      this.tooltipRef.destroy();
+      this.tooltipRef = null;
+    }
+  }
+
+  public openInfoSheet(item: Item, index: number) {
+    this.removeTooltip();
+    this.bottomSheet.open(ItemInfoSheetComponent, {
+      panelClass: 'item-info-sheet',
+      autoFocus: false,
+      backdropClass: 'as-dialog-backdrop',
+      data: { item: item, isOwner: false, fromEquip: true, reclame: false, edit: false }
+    });
+  }
 }
