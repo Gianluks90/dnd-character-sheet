2208c0d030f3545aee6d1fccd8fdd85f1c92f4b9
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2025-01-23 18:00:05 +0100

    Update 23/01/2025
    + char view next resources logic and style;

--- src/app/components/character-view-next/character-view-next-content/sub-components/char-resources/char-next-resources.component.ts
+++ src/app/components/character-view-next/character-view-next-content/sub-components/char-resources/char-next-resources.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { AddResourceDialogComponent } from 'src/app/components/character-view/sub-components/character-view-status/add-resource-dialog/add-resource-dialog.component';
 import { ConditionDialogComponent } from 'src/app/components/utilities/conditions/condition-dialog/condition-dialog.component';
 import { ConditionInfoDialogComponent } from 'src/app/components/utilities/conditions/condition-info-dialog/condition-info-dialog.component';
 import { CharacterService } from 'src/app/services/character.service';
@@ -21,7 +22,7 @@ export class CharNextResourcesComponent {
   @Input() set char(character: any) {
     this._char = character;
     console.log('CharNextResourcesComponent: ', this._char);
-    
+
     setTimeout(() => {
       this.setupResources();
     }, 100);
@@ -34,37 +35,63 @@ export class CharNextResourcesComponent {
 
   private setupResources(): void { }
 
-    public newCondition(): void {
-      this.matDialog.open(ConditionDialogComponent, {
-        width: window.innerWidth < 768 ? '90%' : '500px',
-        autoFocus: false,
-        disableClose: true,
-        backdropClass: 'as-dialog-backdrop',
-        data: {
-          characters: [this._char]
-        }
-      });
-    }
-  
-    public conditionInfo(condition: any): void {
-      this.matDialog.open(ConditionInfoDialogComponent, {
-        width: window.innerWidth < 768 ? '90%' : '500px',
-        autoFocus: false,
-        disableClose: true,
-        backdropClass: 'as-dialog-backdrop',
-        data: {
-          condition: condition
-        }
-      }).afterClosed().subscribe((result: any) => {
-        if (result && result.status === 'delete') {
-          const charConditions = this._char.parametriVitali.conditions;
-          const index = charConditions.findIndex((c: any) => c.name === condition.name);
-          if (index > -1) {
-            charConditions.splice(index, 1);
-            this.charService.updateCharacterConditions(this._char.id, charConditions);
-          }
-        }
-      });
-    }
+  public newCondition(): void {
+    this.matDialog.open(ConditionDialogComponent, {
+      width: window.innerWidth < 768 ? '90%' : '500px',
+      autoFocus: false,
+      disableClose: true,
+      backdropClass: 'as-dialog-backdrop',
+      data: {
+        characters: [this._char]
+      }
+    });
+  }
 
+  public conditionInfo(condition: any): void {
+    this.matDialog.open(ConditionInfoDialogComponent, {
+      width: window.innerWidth < 768 ? '90%' : '500px',
+      autoFocus: false,
+      disableClose: true,
+      backdropClass: 'as-dialog-backdrop',
+      data: {
+        condition: condition
+      }
+    }).afterClosed().subscribe((result: any) => {
+      if (result && result.status === 'delete') {
+        const charConditions = this._char.parametriVitali.conditions;
+        const index = charConditions.findIndex((c: any) => c.name === condition.name);
+        if (index > -1) {
+          charConditions.splice(index, 1);
+          this.charService.updateCharacterConditions(this._char.id, charConditions);
+        }
+      }
+    });
+  }
+
+  public toggleInspiration(): void {
+    this.charService.updateInspiration(this._char.id, !this._char.ispirazione);
+  }
+
+  public addResource(): void {
+    this.matDialog.open(AddResourceDialogComponent, {
+      width: innerWidth < 768 ? '90%' : '500px',
+      autoFocus: false,
+      disableClose: true,
+      backdropClass: 'as-dialog-backdrop',
+      data: {
+        charId: this._char.id
+      }
+    })
+  }
+
+  public resourceAction(action: 'add' | 'remove', index: number): void {
+    const resource = this._char.informazioniBase.risorseAggiuntive[index];
+    if (action === 'add') {
+      resource.valoreAttuale + 1 > resource.valoreMassimo ? resource.valoreAttuale = resource.valoreMassimo : resource.valoreAttuale += 1;
+    } else {
+      resource.valoreAttuale - 1 < 0 ? resource.valoreAttuale = 0 : resource.valoreAttuale -= 1;
+    }
+    this.charService.updateAdditionalResources(this._char.id, this._char.risorseAggiuntive);
+  }
+ 
 }
